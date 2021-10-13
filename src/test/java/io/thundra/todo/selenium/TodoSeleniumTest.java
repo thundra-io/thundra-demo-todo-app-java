52ff196ed36ea0397894f0d947f4f5d6594460da
Tolga Takır <tolga@thundra.io>
2021-10-13 17:43:26 +0300

    Add sample selenium screenshot test

--- src/test/java/io/thundra/todo/selenium/TodoSeleniumTest.java
+++ src/test/java/io/thundra/todo/selenium/TodoSeleniumTest.java
@@ -2,9 +2,16 @@ package io.thundra.todo.selenium;
 
 import io.thundra.todo.ContextInitializedTest;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -18,6 +25,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author tolgatakir
  */
 @Testcontainers
+@SqlGroup({
+        @Sql(scripts = "/sql/clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 class TodoSeleniumTest extends ContextInitializedTest {
 
     @Container
@@ -27,8 +37,45 @@ class TodoSeleniumTest extends ContextInitializedTest {
     @Test
     void testOpenTodoPage() {
         RemoteWebDriver driver = chrome.getWebDriver();
-        driver.get("http://host.testcontainers.internal:" + localPort);
+        driver.get("http://" + getHostAddress() + ":" + localPort);
         List<WebElement> elements = driver.findElementsByTagName("h1");
+        chrome.getWebDriver().getScreenshotAs(OutputType.FILE);
         assertThat(elements).extracting("text").containsOnly("todos");
+    }
+
+    @Test
+    void testAddTodo() {
+        RemoteWebDriver driver = chrome.getWebDriver();
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        driver.get("http://" + getHostAddress() + ":" + localPort);
+        driver.getScreenshotAs(OutputType.BYTES);
+        List<WebElement> elements = driver.findElementsByClassName("new-todo");
+        assertThat(elements).isNotEmpty();
+        WebElement inputElement = elements.get(0);
+
+        inputElement.sendKeys("Test Todo");
+        inputElement.sendKeys(Keys.ENTER);
+        By xpath = By.xpath("/html/body/div/section/section/ul/li[1]/div/label");
+        List<WebElement> assertionElements = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(xpath));
+        driver.getScreenshotAs(OutputType.BASE64);
+        assertThat(assertionElements).extracting("text").containsOnly("Test Todo");
+
+        inputElement.sendKeys("Test Todo2");
+        inputElement.sendKeys(Keys.ENTER);
+        xpath = By.xpath("/html/body/div/section/section/ul/li[2]/div/label");
+        assertionElements = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(xpath));
+        driver.getScreenshotAs(OutputType.FILE);
+        assertThat(assertionElements).extracting("text").containsOnly("Test Todo2");
+    }
+
+    private static String getHostAddress() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.indexOf("nix") >= 0
+                || os.indexOf("nux") >= 0
+                || os.indexOf("aix") > 0) {
+            return "172.17.0.1";
+        } else {
+            return "host.docker.internal";
+        }
     }
 }
