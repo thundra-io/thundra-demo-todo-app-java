9aeb62ce035b3ab027a27ef37ceedcbdb9f8c982
Tolga Takır <tolga@thundra.io>
2021-10-29 20:08:25 +0300

    Test commit pr

--- src/main/java/io/thundra/todo/TodoApplication.java
+++ src/main/java/io/thundra/todo/TodoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TodoApplication.class, args);
+
 	}
 
 }
