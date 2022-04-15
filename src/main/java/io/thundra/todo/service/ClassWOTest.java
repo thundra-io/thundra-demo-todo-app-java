754690aeb836a2e8fbc7b28986d97cd80740a998
Yusuf Erdem <yusuf@thundra.io>
2022-04-15 14:54:41 +0300

    ""add coverage report""

---
+++ src/main/java/io/thundra/todo/service/ClassWOTest.java
@@ -0,0 +1,12 @@
+package io.thundra.todo.service;
+
+/**
+ * @author yusuferdem
+ */
+public class ClassWOTest {
+    public String findString() {
+        String str = "Hello World";
+        str = str.substring(5);
+        return str;
+    }
+}
